03828521b3db837239704b999d34cd179b3a2f22
Vitaliy Prachov <vitaliy.prachov@gmail.com>
2022-02-23 02:47:42 +0200

    feat: Remove non existing lab2

--- src/routes/paths.jsx
+++ src/routes/paths.jsx
@@ -2,7 +2,6 @@ import Lab1 from '../pages/Lab1';
 import Lab2 from '../pages/Lab2';
 
 export const LAB1 = '/lab1';
-export const LAB2 = '/lab2';
 
 export const PAGES = [
   {
@@ -10,11 +9,5 @@ export const PAGES = [
     path: LAB1,
     element: <Lab1 />,
     title: 'Лабораторна робота №1',
-  },
-  {
-    id: LAB2,
-    path: LAB2,
-    element: <Lab2 />,
-    title: 'Лабораторна робота №2',
   },
 ];
